db97e40f14ecd1cf62e49721396a88ad7d7927e6
Kush Patel <kushpatel72@gmail.com>
2012-03-22 15:29:57 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HsSf::Application.config.secret_token = 'bd6c90f08be5901ff37c475107502bdfc8b9c0cbef123ea4c033cf74bbaf79182b68a6d883662f45a763bcdd7e697dda61808fa37ab6ee442f984a05c656f11e'
